d160692718fded007a86fc1479425a21be4897e8
djoko77 <rombie@byui.edu>
2018-11-13 23:45:20 +0800

    commit

--- assignments/lesson-9/scripts/weatherapi.js
+++ assignments/lesson-9/scripts/weatherapi.js
@@ -1,11 +1,12 @@
-var apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=9245458199b42e2a0a9617a7a202fd93&mode=json&units=metric';
+var apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=9245458199b42e2a0a9617a7a202fd93&mode=json';
 var weatherRequest = new XMLHttpRequest();
 weatherRequest.open('GET', apiURL, true);
 weatherRequest.send();
 
 weatherRequest.onload =  function () {
     let weatherData = JSON.parse(weatherRequest.responseText);
-    console.log(weatherData);
-
-    document.getElementById("current-temp").innerHTML = weatherData;
+    console.log(weatherData.main['temp']);
+    var currentTemp = weatherData.main['temp'];
+    var currentTempInF = (currentTemp*(9/5))-459.67;
+    document.getElementById("current-temp").innerHTML = currentTempInF.toFixed(2);
 }
