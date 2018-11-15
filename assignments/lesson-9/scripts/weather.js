2c59f96e866d88cafdd918bf8d1883336076ffd3
djoko77 <rombie@byui.edu>
2018-11-16 02:34:19 +0800

    commit

---
+++ assignments/lesson-9/scripts/weather.js
@@ -0,0 +1,17 @@
+var apiURL = 'https://api.openweathermap.org/data/2.5/weather?id=5604473&APPID=9245458199b42e2a0a9617a7a202fd93&mode=json&units=imperial';
+var weatherRequest = new XMLHttpRequest();
+weatherRequest.open('GET', apiURL, true);
+weatherRequest.send();
+
+weatherRequest.onload =  function () {
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData.main['temp']);
+    var currentTemp = weatherData.main['temp'];
+    document.getElementById("current-temperature").innerHTML = currentTemp+" &deg;F";
+    var currentHumidity = weatherData.main['humidity'];
+    document.getElementById("current-humidity").innerHTML = currentHumidity+"%";
+    var currentPrecipitation = weatherData.main['precipitation'];
+    document.getElementById("current-humidity").innerHTML = currentPrecipitation+" inches";
+    var currentWindSpeed = weatherData.wind['speed'];
+    document.getElementById("wind-speed").innerHTML = currentWindSpeed+" mph";
+}
