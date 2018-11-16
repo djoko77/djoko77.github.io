02df01cedf0e802bdb40c713da2eda3851ae6cc2
djoko77 <rombie@byui.edu>
2018-11-17 04:03:11 +0800

    commit

--- assignments/lesson-9/scripts/weather.js
+++ assignments/lesson-9/scripts/weather.js
@@ -39,13 +39,28 @@ console.log(weatherData.rain);
 
 }
 
-var apiURL = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=9245458199b42e2a0a9617a7a202fd93&mode=json&units=imperial';
-var weatherRequest = new XMLHttpRequest();
-weatherRequest.open('GET', apiURL, true);
-weatherRequest.send();
+var apiURL2 = 'https://api.openweathermap.org/data/2.5/forecast?id=5604473&APPID=9245458199b42e2a0a9617a7a202fd93&mode=json&units=imperial';
+var weatherRequest2 = new XMLHttpRequest();
+weatherRequest2.open('GET', apiURL2, true);
+weatherRequest2.send();
 
-weatherRequest.onload =  function () {
-    let weatherForecast = JSON.parse(weatherRequest.responseText);
-console.log(weatherData.rain);
+weatherRequest2.onload =  function () {
+    let weatherForecast = JSON.parse(weatherRequest2.responseText);
+console.log(weatherForecast.list[0].main["temp_max"]);
 
+var HTday1 = weatherForecast.list[0].main["temp_max"];
+document.getElementById("HT1").innerHTML = HTday1+" &deg;F";
+
+var HTday2 = weatherForecast.list[1].main["temp_max"];
+document.getElementById("HT2").innerHTML = HTday2+" &deg;F";
+
+var HTday3 = weatherForecast.list[2].main["temp_max"];
+document.getElementById("HT3").innerHTML = HTday3+" &deg;F";
+
+var HTday4 = weatherForecast.list[3].main["temp_max"];
+document.getElementById("HT4").innerHTML = HTday4+" &deg;F";
+
+var HTday5 = weatherForecast.list[4].main["temp_max"];
+document.getElementById("HT5").innerHTML = HTday5+" &deg;F";
+}
 
