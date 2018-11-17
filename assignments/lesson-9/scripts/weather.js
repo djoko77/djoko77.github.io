d4902aa4c57854c916a95c979861ecf0dbc708e5
djoko77 <rombie@byui.edu>
2018-11-17 13:01:13 +0800

    commit

--- assignments/lesson-9/scripts/weather.js
+++ assignments/lesson-9/scripts/weather.js
@@ -46,21 +46,60 @@ weatherRequest2.send();
 
 weatherRequest2.onload =  function () {
     let weatherForecast = JSON.parse(weatherRequest2.responseText);
-console.log(weatherForecast.list[0].main["temp_max"]);
+console.log(weatherForecast.list[0].dt_txt);
 
+/* DAY ONE */
 var HTday1 = weatherForecast.list[0].main["temp_max"];
 document.getElementById("HT1").innerHTML = HTday1+" &deg;F";
 
-var HTday2 = weatherForecast.list[1].main["temp_max"];
+var HTdayName0 = weatherForecast.list[0].dt_txt;
+var dateForecast0 = new Date(HTdayName0);
+var d = new Array(7);
+    d[0] = "Sunday";
+    d[1] = "Monday";
+    d[2] = "Tuesday";
+    d[3] = "Wednesday";
+    d[4] = "Thursday";
+    d[5] = "Friday";
+    d[6] = "Saturday";
+var d0 = d[dateForecast0.getDay()];
+document.getElementById("dailyForecast0").innerHTML = d0;
+
+/* DAY TWO */
+var HTday2 = weatherForecast.list[6].main["temp_max"];
 document.getElementById("HT2").innerHTML = HTday2+" &deg;F";
 
-var HTday3 = weatherForecast.list[2].main["temp_max"];
+var HTdayName1 = weatherForecast.list[6].dt_txt;
+var dateForecast1 = new Date(HTdayName1);
+var d1 = d[dateForecast1.getDay()];
+document.getElementById("dailyForecast1").innerHTML = d1;
+
+/* DAY THREE */
+var HTday3 = weatherForecast.list[14].main["temp_max"];
 document.getElementById("HT3").innerHTML = HTday3+" &deg;F";
 
-var HTday4 = weatherForecast.list[3].main["temp_max"];
+var HTdayName2 = weatherForecast.list[14].dt_txt;
+var dateForecast2 = new Date(HTdayName2);
+var d2 = d[dateForecast2.getDay()];
+document.getElementById("dailyForecast2").innerHTML = d2;
+
+/* DAY FOUR */
+var HTday4 = weatherForecast.list[22].main["temp_max"];
 document.getElementById("HT4").innerHTML = HTday4+" &deg;F";
 
-var HTday5 = weatherForecast.list[4].main["temp_max"];
+var HTdayName3 = weatherForecast.list[22].dt_txt;
+var dateForecast3 = new Date(HTdayName3);
+var d3 = d[dateForecast3.getDay()];
+document.getElementById("dailyForecast3").innerHTML = d3;
+
+/* DAY FIVE */
+var HTday5 = weatherForecast.list[30].main["temp_max"];
 document.getElementById("HT5").innerHTML = HTday5+" &deg;F";
+
+var HTdayName4 = weatherForecast.list[30].dt_txt;
+var dateForecast4 = new Date(HTdayName4);
+var d4 = d[dateForecast4.getDay()];
+document.getElementById("dailyForecast4").innerHTML = d4;
+
 }
 
