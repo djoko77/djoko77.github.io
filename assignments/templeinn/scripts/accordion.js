e5c6b17a2ba8f44944c7bf032397c33fac5ddc26
djoko77 <rombie@byui.edu>
2018-12-16 17:30:57 +0800

    commit

--- assignments/templeinn/scripts/accordion.js
+++ assignments/templeinn/scripts/accordion.js
@@ -1,18 +1,14 @@
-var acc = document.getElementsByClassName("accordion");
-var i;
+/* $(function(){
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "active" class,
-    to highlight the button that controls the panel */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
+  $(".accordion").on({
+    click: function(){
+      $(this).toggle("active");
     }
+  }, ".panel")
+}) */
+
+$(function() {
+  $(".accordion").click(function() {
+      $(".panel").slideToggle(300);
   });
-}
+}); 
