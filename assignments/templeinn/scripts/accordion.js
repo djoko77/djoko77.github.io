7ba7ccd2d842e67a852e1aad76bd02bd90f7b598
djoko77 <rombie@byui.edu>
2018-12-19 11:12:11 +0800

    commit

--- assignments/templeinn/scripts/accordion.js
+++ assignments/templeinn/scripts/accordion.js
@@ -20,18 +20,3 @@ $(function() {
 });
 })
 */
-var acc = $("accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-acc[i].click(function() {
-$(this).toggle("active");
-
-var panel = $(this).next();
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-} 
