10c6d7ccacd9c98c45b9beb816bca75c1572f4de
djoko77 <rombie@byui.edu>
2018-12-20 00:30:30 +0800

    commit

--- assignments/templeinn/scripts/accordion.js
+++ assignments/templeinn/scripts/accordion.js
@@ -1,22 +1,20 @@
-/* $(function(){
-
-  $(".accordion").on({
-    click: function(){
-      $(this).toggle("active");
+$(document).ready(function(){
+  $(".set > button").on("click", function(){
+    if($(this).hasClass('active')){
+      $(this).removeClass("active");
+      $(this).siblings('.panel').slideUp(300);
+      $(".set > button i").removeClass("fa-minus").addClass("fa-plus");
+    }else{
+      $(".set > button i").removeClass("fa-minus").addClass("fa-plus");
+    $(this).find("i").removeClass("fa-plus").addClass("fa-minus");
+    $(".set > button").removeClass("active");
+    $(this).addClass("active");
+    $('.panel').slideUp(200);
+    $(this).siblings('.panel').slideDown(300);
     }
-  }, ".panel")
-})
 
-  $('.accordian').click(function() {
-      $('.panel').slideToggle(500);
   });
-
-  var acc = document.getElementsByClassName("accordion");
-var i;
-
-$(function() {
-  $(".accordian").click(function() {
-  $('.panel').slideToggle(500);
 });
-})
-*/
+
+
+
